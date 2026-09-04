baf09f91114e75715ff3fa0e13da3e00798fd4b3
sriyuth <sriyuthk122@gmail.com>
2026-09-04 17:59:55 +0530

    Delete xxx.py

--- xxx.py
+++
@@ -1,8 +0,0 @@
-def add_underscores(word):
-new_word = "_" # Initialize with an underscore
-for char in word:
-new_word = char + "_" # Overwrites new_word instead of appending
-return new_word
-
-phrase = "hello"
-print(add_underscores(phrase))
